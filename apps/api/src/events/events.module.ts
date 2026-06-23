e58e68882105238e2dd849cc30b7c80fa2021687
Muhammed Aslan <eng.muhammedaslan201999@gmail.com>
2026-06-23 19:31:19 +0300

    feat(events): fire notifications on key event state transitions

--- apps/api/src/events/events.module.ts
+++ apps/api/src/events/events.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
 import { EventStateService } from './event-state.service';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+  imports: [NotificationsModule],
   controllers: [EventsController],
   providers: [EventsService, EventStateService],
   exports: [EventsService, EventStateService],
